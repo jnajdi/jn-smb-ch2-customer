0527028952246524df6d6b4594e47163c0cf1fe3
Jihane Najdi <jnajdi@gmail.com>
2017-03-12 08:45:32 -0700

    Initial Commit

---
+++ src/main/java/com/example/JnSmbCh2CustomerApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnSmbCh2CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnSmbCh2CustomerApplication.class, args);
+	}
+}
